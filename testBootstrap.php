1a2dc5d1089f2b89528dd49b336b6142c4fb72a9
Jeremy Booker <jeremy.booker@gmail.com>
2024-02-11 00:12:05 -0500

    Add testBoostrap for phpstan setup

---
+++ testBootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+define('PHPWS_SOURCE_DIR', './');
+
+registerAutoloaders();
+
+function registerAutoloaders()
+{
+
+    include PHPWS_SOURCE_DIR . 'Autoloaders/Autoloaders.config.php';
+
+    if (!isset($autoloaders)) {
+        throw new \Exception('Missing autoloader configuration file.');
+    }
+
+    if (!is_array($autoloaders) || empty($autoloaders)) {
+        throw new \Exception('Autoloader configuration not formatted correctly.');
+    }
+
+    foreach ($autoloaders as $file => $autoload_function) {
+        require_once PHPWS_SOURCE_DIR . 'Autoloaders/' . $file . '.php';
+        if (!empty($autoload_function)) {
+            spl_autoload_register($autoload_function);
+        }
+    }
+}
