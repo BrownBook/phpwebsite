6d4ef0926f04dea605674f77bcdb8aed07b0360d
Jeremy Booker <jeremy.booker@gmail.com>
2024-09-02 10:36:16 -0400

    More strftime updates for PHP 8.1

--- mod/blog/conf/config.php
+++ mod/blog/conf/config.php
@@ -5,7 +5,7 @@
  * @author Matthew McNaney <mcnaney at gmail dot com>
  */
 
-define('BLOG_VIEW_DATE_FORMAT', '%l:%M%P on %A, %B %e %Y');
-define('BLOG_PAGER_DATE_FORMAT', '%R - %e %b, %Y');
+define('BLOG_VIEW_DATE_FORMAT', 'g:ia on l, F j Y');
+define('BLOG_PAGER_DATE_FORMAT', 'G:i - j M, Y');
 define('MAX_BLOG_CACHE_PAGES', 3);
 
