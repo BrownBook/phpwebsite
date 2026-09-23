57949e0ef9b9abd9613921d4a1c5e50e7f53cd38
Jeremy Booker <jeremy.booker@gmail.com>
2026-09-23 13:24:25 -0400

    fix js bug in pageEdit scripts

--- mod/pagesmith/javascript/pageedit/script.js
+++ mod/pagesmith/javascript/pageedit/script.js
@@ -5,114 +5,128 @@ var current_block;
 var formSubmitting = true;
 $(document).ready(function () {
   checkUpdateBeforeSubmit();
-  var editor = CKEDITOR.replace('block-edit-textarea', {height: '450'});
+  var editor = CKEDITOR.replace("block-edit-textarea", { height: "450" });
 
   /**
    * This serves two purposes.
    * 1) Makes the save button appear as it normally
    */
-  editor.on('instanceReady', function () {
-    editor.addCommand('save', {
+  editor.on("instanceReady", function () {
+    editor.addCommand("save", {
       modes: {
         wysiwyg: 1,
-        source: 1
+        source: 1,
       },
       exec: function () {
         updateBlock(editor);
         if (editor.commands.maximize.state == 1) {
-          editor.execCommand('maximize');
+          editor.execCommand("maximize");
         }
-        $('#edit-section').modal('hide');
-      }
+        $("#edit-section").modal("hide");
+      },
     });
   });
 
-  enforceFocus();
+  //enforceFocus();
   localStorage.clear();
   initializeDialog(editor);
   initializePageTitleEdit();
   editBlock(editor);
-  $('#page-title-edit').popover({
+  $("#page-title-edit").popover({
     html: true,
-    placement: 'auto',
-    trigger: 'hover',
-    content: '<span style="margin:0px;padding:0px;font-size:16px;font-weight:bold">Click on ' +
-        'title to edit</span>'
+    placement: "auto",
+    trigger: "hover",
+    content:
+      '<span style="margin:0px;padding:0px;font-size:16px;font-weight:bold">Click on ' +
+      "title to edit</span>",
   });
-  $('.block-edit').popover(
-    {html: true, placement: 'auto', trigger: 'hover', content: '<span style="font-size:16px;font-weight:bold">Click on text to edit</span>'}
-  );
-
+  $(".block-edit").popover({
+    html: true,
+    placement: "auto",
+    trigger: "hover",
+    content:
+      '<span style="font-size:16px;font-weight:bold">Click on text to edit</span>',
+  });
 });
 function editBlock(editor) {
-  $('.block-edit').click(function () {
+  $(".block-edit").click(function () {
     current_block = $(this);
-    block_id = $(this).data('block-id');
-    page_id = $(this).data('page-id');
-    section_id = $(this).attr('id');
+    block_id = $(this).data("block-id");
+    page_id = $(this).data("page-id");
+    section_id = $(this).attr("id");
     if (localStorage[block_id] !== undefined) {
       editor.setData(localStorage[block_id]);
       openBlockEdit();
     } else {
-      $.get('index.php', {
-        'module': 'pagesmith',
-        'aop': 'block_info',
-        'pid': page_id,
-        'bid': block_id,
-        'section_id': section_id
-      }, function (data) {
-        editor.setData(data);
-        openBlockEdit();
-      });
+      $.get(
+        "index.php",
+        {
+          module: "pagesmith",
+          aop: "block_info",
+          pid: page_id,
+          bid: block_id,
+          section_id: section_id,
+        },
+        function (data) {
+          editor.setData(data);
+          openBlockEdit();
+        },
+      );
     }
   });
 }
 
 function enforceFocus() {
   $.fn.modal.Constructor.prototype._enforceFocus = function () {
-    $(document).on('focusin', function (e) {
-      if ((this.$element && this.$element[0] !== e.target) && !this.$element.has(e.target).length && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select') && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')) {
-        this.$element.focus()
+    $(document).on("focusin", function (e) {
+      if (
+        this.$element &&
+        this.$element[0] !== e.target &&
+        !this.$element.has(e.target).length &&
+        !$(e.target.parentNode).hasClass("cke_dialog_ui_input_select") &&
+        !$(e.target.parentNode).hasClass("cke_dialog_ui_input_text")
+      ) {
+        this.$element.focus();
       }
-    })
+    });
   };
 }
 
 function openBlockEdit() {
-  $('#edit-section').modal('show');
+  $("#edit-section").modal("show");
 }
 
 function openTitleEdit() {
-  $('#edit-title').modal('show');
+  $("#edit-title").modal("show");
 }
 
 function initializePageTitleEdit() {
-  $('#page-title-edit').click(function () {
-    if (!$('#page-title-edit').data('new')) {
-      $('#page-title-input').val($('#page-title-edit').html());
+  $("#page-title-edit").click(function () {
+    if (!$("#page-title-edit").data("new")) {
+      $("#page-title-input").val($("#page-title-edit").html());
     }
     openTitleEdit();
   });
 
-  $('#save-title').click(function () {
-    var title_input = $('#page-title-input').val();
-    title_input = title_input.replace('/[<>]/gi', '');
-    $('#page-title-hidden').val(title_input);
-    $('#page-title-edit').html(title_input);
-    $('#page-title-edit').css('color', 'inherit');
-    $('#edit-title').modal('hide');
+  $("#save-title").click(function () {
+    var title_input = $("#page-title-input").val();
+    title_input = title_input.replace("/[<>]/gi", "");
+    $("#page-title-hidden").val(title_input);
+    $("#page-title-edit").html(title_input);
+    $("#page-title-edit").css("color", "inherit");
+    $("#edit-title").modal("hide");
   });
 }
 
 function initializeDialog(editor) {
-  $('#edit-section').on('hide.bs.modal', function (e) {
+  $("#edit-section").on("hide.bs.modal", function (e) {
     ck_data = editor.getData();
     localStorage[block_id] = ck_data;
   });
 
-  $('#save-page').click(function () {
+  $("#save-page").click(function () {
     updateBlock(editor);
-    $('#edit-section').modal('hide');
+    $("#edit-section").modal("hide");
   });
 }
 
@@ -120,19 +134,23 @@ function updateBlock(editor) {
   formSubmitting = false;
   localStorage.removeItem(block_id);
   content = editor.getData();
-  $.post('index.php', {
-    'module': 'pagesmith',
-    'aop': 'save_block',
-    'pid': page_id,
-    'bid': block_id,
-    'content': content,
-    'section_id': section_id
-  }, function (data) {
-    if (content === '') {
-      content = '[Click to edit]';
-    }
-    current_block.html(content);
-  });
+  $.post(
+    "index.php",
+    {
+      module: "pagesmith",
+      aop: "save_block",
+      pid: page_id,
+      bid: block_id,
+      content: content,
+      section_id: section_id,
+    },
+    function (data) {
+      if (content === "") {
+        content = "[Click to edit]";
+      }
+      current_block.html(content);
+    },
+  );
 }
 
 function submittingForm(e) {
@@ -145,7 +163,8 @@ function checkUpdateBeforeSubmit() {
     if (formSubmitting) {
       return undefined;
     }
-    var confirmationMessage = 'If you leave before saving, your changes will be lost.';
+    var confirmationMessage =
+      "If you leave before saving, your changes will be lost.";
 
     (e || window.event).returnValue = confirmationMessage; //Gecko + IE
     return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
@@ -159,25 +178,21 @@ jQuery.fn.center = function (absolute) {
   return this.each(function () {
     var t = jQuery(this);
     t.css({
-      position: absolute
-        ? 'absolute'
-        : 'fixed',
-      left: '50%',
-      top: '50%',
-      zIndex: '99'
+      position: absolute ? "absolute" : "fixed",
+      left: "50%",
+      top: "50%",
+      zIndex: "99",
     }).css({
-      marginLeft: '-' + (
-        t.outerWidth() / 2
-      ) + 'px',
-      marginTop: '-' + (
-        t.outerHeight() / 2
-      ) + 'px'
+      marginLeft: "-" + t.outerWidth() / 2 + "px",
+      marginTop: "-" + t.outerHeight() / 2 + "px",
     });
     if (absolute) {
       t.css({
-        marginTop: parseInt(t.css('marginTop'), 10) + jQuery(window).scrollTop(),
-        marginLeft: parseInt(t.css('marginLeft'), 10) + jQuery(window).scrollLeft()
-      })
+        marginTop:
+          parseInt(t.css("marginTop"), 10) + jQuery(window).scrollTop(),
+        marginLeft:
+          parseInt(t.css("marginLeft"), 10) + jQuery(window).scrollLeft(),
+      });
     }
-  })
+  });
 };
